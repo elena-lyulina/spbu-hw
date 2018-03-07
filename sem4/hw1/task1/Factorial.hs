02e08b32e60dc7e00e3096e6cf2e6cb3c6f75426
elena-lyulina <elena.lyulina@gmail.com>
2018-03-07 13:51:23 +0300

    fixed typo

--- sem4/hw1/task1/Factorial.hs
+++ sem4/hw1/task1/Factorial.hs
@@ -3,4 +3,4 @@ module Factorial where
 factorial :: Integer -> Integer
 factorial n | n == 0    = 1
             | n > 0     = n * factorial (n - 1)
-            | otherwise = error "arg must be > 0"
+            | otherwise = error "arg must be >= 0"
