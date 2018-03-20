1f7ca5d1726ced0cff45797341e14f0dbc513454
elena-lyulina <elena.lyulina@gmail.com>
2018-03-20 09:12:23 +0300

    that was soo stupid

--- sem4/hw2/task5/2_5.hs
+++ sem4/hw2/task5/2_5.hs
@@ -3,7 +3,7 @@ sum' a b c = helper a b c [] where
              helper a b c res | allNull a b c = reverse res
                               | otherwise     = helper (tailOrNull a) (tailOrNull b) (tailOrNull c) ((headOr0 a + headOr0 b + headOr0 c):res)
 
-             allNull a b c = if (a == [] && b == [] && c == c) then True else False
+             allNull a b c = (a == [] && b == [] && c == [])
              tailOrNull a = if (a == []) then [] else (tail a)
              --should i worry about arithmetic overflows?
              headOr0 a = if (a == []) then 0 else (toInteger (head a))
